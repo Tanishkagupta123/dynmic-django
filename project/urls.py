6d34b3cd12c8ec2afc6551beecf52a8cf9dc2544
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-06-06 17:42:06 +0530

    first commit

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,6 @@ from app.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',landing,name='landing'),
